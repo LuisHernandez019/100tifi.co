808457f7ab79e40132a1931f3dd27ec007e776a6
Luis <uwu.luishernandez@gmail.com>
2021-08-18 20:20:17 -0500

    Decorete styles

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -5,20 +5,22 @@ const Character = async () => {
    const id = getHash();
    const character = await getData(id);
    const view = `
-      <div class="Character-inner">
-         <article class="Character-card">
+      <section class="character-inner">
+         <article class="character-card">
             <img src="${character.image}" alt="${character.name}">
-            <h2>${character.name}</h2>
+            <div class="portal-container">
+               <h2 class="character-name">${character.name}</h2>
+            </div>
          </article>
-         <article class="Characters-card">
-            <h3>Episodes: <span>${character.episode.length}</span></h3>
-            <h3>Status: <span>${character.status}</span></h3>
-            <h3>Species: <span>${character.species}</span></h3>
-            <h3>Gender: <span>${character.gender}</span></h3>
-            <h3>Origin: <span>${character.origin.name}</span></h3>
-            <h3>Last Location: <span>${character.location.name}</span></h3>
+         <article class="character-description">
+            <h3>• Episodes: <span>${character.episode.length}</span></h3>
+            <h3>• Status: <span>${character.status}</span></h3>
+            <h3>• Species: <span>${character.species}</span></h3>
+            <h3>• Gender: <span>${character.gender}</span></h3>
+            <h3>• Origin: <span>${character.origin.name}</span></h3>
+            <h3>• Last Location: <span>${character.location.name}</span></h3>
          </article>
-      </div>
+      </section>
    `;
    return view;
 };
